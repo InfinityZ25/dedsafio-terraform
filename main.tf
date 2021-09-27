c7839b45fc71ebf288da72e1a4534973d17f4515
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-09-27 12:00:52 -0400

    Functional stage reached

--- main.tf
+++ main.tf
@@ -37,16 +37,38 @@ resource "digitalocean_droplet" "dedsafio-droplet" {
     private_key = file(var.private_key)
     timeout     = "2m"
   }
+  # Create the folders for the deployment 
   provisioner "remote-exec" {
     inline = [
       # Create the /home/minecraft directory
       "mkdir /home/minecraft",
       "cd /home/minecraft",
       # Create all the directories and make them accessible for any user.
-      "mkdir -p proxy server1 server2",
-      "chmod -R 777 /home/minecraft/*",
+      "mkdir -m 777 -p minecraft-data/proxy minecraft-data/server1 minecraft-data/server2",
       # Obtain the docker-compose file
       "wget https://gist.githubusercontent.com/InfinityZ25/cf2c7bc880a702af565032582b481c47/raw/7e21d68d8f8b158ed6b162b07f87558a8f91f906/docker-compose.yml",
+    ]
+  }
+  # Copy the files to the proxy
+  provisioner "file" {
+    source      = "images/dedsafio-proxy/"
+    destination = "/home/minecraft/minecraft-data/proxy"
+  }
+  # Copy the files to the server1
+  provisioner "file" {
+    source      = "images/dedsafio-server/"
+    destination = "/home/minecraft/minecraft-data/server1"
+  }
+  # Copy the files to the server2
+  provisioner "file" {
+    source      = "images/dedsafio-server/"
+    destination = "/home/minecraft/minecraft-data/server2"
+  }
+  # Run the docker-compose command
+  provisioner "remote-exec" {
+    inline = [
+      # Change directories to the /home/minecraft directory
+      "cd /home/minecraft",
       # Start the docker-compose process
       "docker-compose up -d",
     ]
